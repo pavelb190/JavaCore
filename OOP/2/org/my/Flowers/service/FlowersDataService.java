f97e394fcd117ffbf1ef10f8cd4d11270706b690
190190pasha <barkovskiy90@gmail.com>
2015-11-06 16:18:37 +0300

    FlowersDataService class updated: method 'persist' added.

--- OOP/2/org/my/Flowers/service/FlowersDataService.java
+++ OOP/2/org/my/Flowers/service/FlowersDataService.java
@@ -5,16 +5,25 @@ import plants.flower.Flower;
 import plants.flower.room.*;
 import plants.flower.garden.*;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import java.util.List;
 import java.util.ArrayList;
+
+import persistent.serialize.FlowerSerializer;
 
 
 public class FlowersDataService {
 
+	public static final String DATA_STORAGE_SRC = "./data.ser";
+
 	public static List<? extends Flower> createFlowersList()
 	{
 		return new ArrayList<Flower>() {{
-			
+
 			//Begonias:
 			add(new Begonia());
 			add(new Begonia());
@@ -31,5 +40,23 @@ public class FlowersDataService {
 			add(new Rose());
 			add(new Rose());
 		}};
+	}
+
+	public static void persist(List<? extends Flower> flowers) throws IOException, FileNotFoundException
+	{
+		FlowerSerializer serializer =
+			new FlowerSerializer(
+				//Save to ..
+				new FileOutputStream(DATA_STORAGE_SRC),
+					//get from ..
+					new FileInputStream(DATA_STORAGE_SRC)
+			);
+
+		for(Flower flower : flowers) {
+
+			serializer.serialize(flower);
+		}
+
+		serializer.clear();
 	}
 }
