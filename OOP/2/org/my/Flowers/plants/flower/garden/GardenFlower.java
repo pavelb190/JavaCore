92a790d0408a773c0bce63f4cc47e0b5865af00b
190190pasha <barkovskiy90@gmail.com>
2015-11-06 16:49:20 +0300

    Method 'run' implemented in basic GardenFlower class.

--- OOP/2/org/my/Flowers/plants/flower/garden/GardenFlower.java
+++ OOP/2/org/my/Flowers/plants/flower/garden/GardenFlower.java
@@ -6,15 +6,26 @@ import plants.flower.FlowerType;
 import plants.flower.TakingCare;
 import plants.flower.florist.ToWeedGrass;
 
+import java.util.Random;
+
 /**
 * Specifies only the garden flowers kind.
 */
 public class GardenFlower extends Flower implements TakingCare<ToWeedGrass> {
 	
+	private static Random rGenerator = new Random();
+
 	private int weedAmount = 0;
 
 	public GardenFlower(final String latinName, String title, FlowerType type) {
 		super(latinName, title, type);
+	}
+
+	public void run()
+	{
+		takeCare(new ToWeedGrass(rGenerator.nextInt(10) + 1));
+
+		Thread.sleep(750); grow();
 	}
 
 	public void takeCare(ToWeedGrass care) {
