9b5a9f2e5a65584cc3891285b1bcce0203769a80
190190pasha <barkovskiy90@gmail.com>
2015-11-30 15:50:30 +0300

    Logger added. (initial)

---
+++ PatternsAndOther/SimpleLogger/Logger.java
@@ -0,0 +1,17 @@
+
+public class Logger {
+
+	private static Logger instance = null;
+
+	private Logger() {}
+
+	public static Logger getInstance() {
+
+		if (instance == null) {
+
+			instance = new Logger();
+		}
+
+		return instance;
+	}
+}
