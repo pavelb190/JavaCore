bd316e807a210db84cc1853483f3d4218ba5df70
190190pasha <barkovskiy90@gmail.com>
2015-12-07 17:28:08 +0300

    UserRole-entity added.

---
+++ MyFinalProject/PaymentsPrj/Model/UserRole.java
@@ -0,0 +1,16 @@
+
+
+public class UserRole extends Entity {
+
+	private String role;
+
+	public String getRole() {
+
+		return this.role;
+	}
+
+	public void setRole(final String role) {
+
+		this.role = role;
+	}
+}
