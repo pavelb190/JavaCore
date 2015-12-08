679326ef340c7cd9c2c346443ca2bc95faadb755
190190pasha <barkovskiy90@gmail.com>
2015-12-08 10:53:53 +0300

    BankAccountDAO-interface modified.

--- MyFinalProject/PaymentsPrj/Model/dao/BankAccountDao.java
+++ MyFinalProject/PaymentsPrj/Model/dao/BankAccountDao.java
@@ -1,10 +1,14 @@
 
+import java.util.List;
+
 import java.sql.SQLException;
 
 
 public interface BankAccountDao {
 
 	public void createAccountForUser(BankAccount account, User user) throws SQLException;
+	public BankAccount getAccountById(final long accountId) throws SQLException;
+	public List<BankAccount> getAccountsForUser(User user) throws SQLException;
 	public void accountUpdate(BankAccount account) throws SQLException;
 	public void accountLock(BankAccount account) throws SQLException;
 	public void accountUnlock(BankAccount account) throws SQLException;
