0fc3bad472840797ee989a51211550d2183fa81a
190190pasha <barkovskiy90@gmail.com>
2015-12-10 09:35:00 +0300

    UserDAO-interface changed.

--- MyFinalProject/PaymentsPrj/Model/dao/UserDao.java
+++ MyFinalProject/PaymentsPrj/Model/dao/UserDao.java
@@ -6,7 +6,7 @@ public interface UserDao {
 	
 	public void addUser(final User newUser) throws SQLException;
 	public User getUserById(final long userId) throws SQLException;
-	public User getUserByName(final String username) throws SQLException;
+	public User getUserByEmail(final String email) throws SQLException;
 	public boolean checkUser(User user) throws SQLException;
 	public boolean checkPassword(final User user, final String password) throws SQLException;
 	public boolean hasRole(final String role) throws SQLException;
