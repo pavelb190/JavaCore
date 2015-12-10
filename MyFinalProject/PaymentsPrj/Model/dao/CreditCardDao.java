4202d856521722ef19fdce6422f86146053dcfbf
190190pasha <barkovskiy90@gmail.com>
2015-12-10 09:37:18 +0300

    CreditCardDAO-interface modified.

--- MyFinalProject/PaymentsPrj/Model/dao/CreditCardDao.java
+++ MyFinalProject/PaymentsPrj/Model/dao/CreditCardDao.java
@@ -6,7 +6,7 @@ public interface CreditCardDao {
 
 	public void addCreditCard(final CreditCard creditCard) throws SQLException;
 	public CreditCard getCreditCardById(final long creditCardId) throws SQLException;
-	public List<CreditCard> getCreditCardsForAccount(BankAccount account) throws SQLException;
+	public List<CreditCard> getCreditCardsForAccount(final BankAccount account) throws SQLException;
 
 	// ...
 }
